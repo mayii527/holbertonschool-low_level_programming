982b0ac90fad75828077f72d62fb479b94c75e0d
mayii527 <3434@holbertonschool.com>
2021-06-21 18:27:45 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - Write a function that computes the absolute value of an integer.
+ *
+ * Return: Absolute value of an integer.
+ */
+int _abs(int n);
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
