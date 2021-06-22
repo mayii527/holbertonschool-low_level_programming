3bee1f558ced579bdf581d80a2fd23f33c34306a
mayii527 <3434@holbertonschool.com>
2021-06-22 08:12:35 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 /**
  * _islower - Write a function that checks for lowercase character.
  * @c: The character to be checked.
+ *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(int c);
