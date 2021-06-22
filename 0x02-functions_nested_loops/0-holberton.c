f2b9970321bedaf1ad0d71bfd3c2d79d962efa7f
mayii527 <3434@holbertonschool.com>
2021-06-21 17:10:48 -0700

    correccion de espacios

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,4 @@
 #include "holberton.c"
-
 /**
  * main - Write a program that prints Holberton, followed by a new line.
  *
@@ -7,14 +6,14 @@
  */
 int main(void)
 {
-  _putchar('H')
-    _putchar('o')
-    _putchar('l')
-    _putchar('b')
-    _putchar('e')
-    _putchar('r')
-    _putchar('t')
-    _putchar('o')
-    _putchar('n')
-    _putchar('\n')
+_putchar('H')
+_putchar('o')
+_putchar('l')
+_putchar('b')
+_putchar('e')
+_putchar('r')
+_putchar('t')
+_putchar('o')
+_putchar('n')
+_putchar('\n')
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "holberton.c"
+/**
+ * main - Write a function that prints the alphabet, in lowercase.
+ */
+void print_alphabet(void)
+{
+char(letter)
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter)
+_putchar('\n')
+}
