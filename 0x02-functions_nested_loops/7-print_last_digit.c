f7152bc0f95be69807930e8f8c2978ba62bd45aa
mayii527 <3434@holbertonschool.com>
2021-06-22 09:58:14 -0700

    last_digit1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,15 @@
 int print_last_digit(int n)
 {
 int last_digit;
-last_digit = n % 10;
-return (last_digit);
+int num_positive;
+if (n <= -1)
+{
+num_positive = n * -1;
+}
+else
+{
+num_positive = n;
+}
+last_digit = num_positive % 10;
+_putchar(last_digit);
 }
