eb371f602e498e589386f3be847078c2eb83147f
mayii527 <3434@holbertonschool.com>
2021-06-21 17:28:02 -0700

    correccion de espacios

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-_putchar('H')
-_putchar('o')
-_putchar('l')
-_putchar('b')
-_putchar('e')
-_putchar('r')
-_putchar('t')
-_putchar('o')
-_putchar('n')
-_putchar('\n')
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 }
