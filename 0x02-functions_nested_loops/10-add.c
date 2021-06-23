4192b9dd269b65e695b0c6fb951d7de062f7254d
mayii527 <3434@holbertonschool.com>
2021-06-23 12:40:12 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * add - Write a function that adds two integers.
+ * @1num: The first integer.
+ * @2num: The second integer.
+ *
+ * Return: The result of the addition.
+ */
+int add(int 1num, int 2num)
+{
+	return (1num + 2num);
+}
