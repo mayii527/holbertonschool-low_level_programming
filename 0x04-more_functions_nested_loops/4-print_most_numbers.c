e296c1f7eeb35ce83281393d82fa04bf05f234aa
mayii527 <3434@holbertonschool.com>
2021-06-25 10:59:00 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Print numbers from 0 to 9, except 2 and 4
+ *
+ * Return: No return value
+ */
+void print_most_numbers(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+if (n != '2' && n != '4')
+_putchar(i);
+_putchar('\n');
+}
