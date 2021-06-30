e75561bb25d58572dfb6b02c723d35b98ea87628
mayii527 <3434@holbertonschool.com>
2021-06-30 09:04:16 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - Print n elements from an integer array
+ * @a: Pointer to the integer array
+ * @n: Number of elements to extract
+ *
+ * Return: Nothing
+*/
+void print_array(int *a, int n)
+{
+int n;
+for (n = 0; n < n; n++)
+{
+if (n == 0)
+printf("%d", *(a + n));
+else
+printf(", %d", *(a + n));
+}
+printf("\n");
+}
