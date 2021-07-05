1c966c4c12ce329dc66fdbfd7f11bf661f6b24d7
mayii527 <3434@holbertonschool.com>
2021-07-05 10:34:00 -0700

    1...

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,10 +1,19 @@
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-  
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char);
+char *_memset(char *, char , unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*)[]);
+void print_diagsums(int *, int);
+void set_string(char **, char *);
+int strleng (char *);
+
+
+#endif /* HOLBERTON_H */  
