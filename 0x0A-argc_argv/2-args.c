3d45433cbf2dab784a6d16043b2f5dc97907425d
mayii527 <3434@holbertonschool.com>
2021-07-09 20:47:39 -0700

    2_args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Prints the number of arguments passed
+ * @argc: Number of arguments passed
+ * @argv: Pointer to array of arguments passed
+ *
+ * Return: 0 - Success
+ */
+
+int main(int argc, char *argv[])
+{
+int a;
+for (a = 0; a < argc; a++)
+printf("%s\n", argv[a]);
+return (0);
+}
