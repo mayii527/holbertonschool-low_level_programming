0108178fe998d3a56b34bce0742a219cd7161400
Mayii Cadavid <3434@holbertonschool.com>
2021-07-22 11:32:28 -0500

    punto 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,7 @@
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+    printf("%d", argc);
+    return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+/**
+ * op_add - sum
+ * @a:
+ */
+
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - rest
+ * @a:
+ */
+
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
