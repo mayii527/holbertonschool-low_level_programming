ab18dbf1fe228c520ab1442c8f3e6fe724a404fc
mayii527 <3434@holbertonschool.com>
2021-08-04 08:04:23 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,20 +2,23 @@
 /**
  * list_len - that returns the number of elements in a linked.
  * @h: structure
- * 
  * Return: number  of the elements
  */
-
 size_t list_len(const list_t *h)
 {
-    size_t i;
-    for (i = 0; h != NULL; i++)
-    {
-        if (h->str == NULL)
-        {
-            h->str = "(nil)";
-        }
-        h = h->next;
-    }
-    return (i);
+int i;
+char *a;
+const list_t *b = NULL;
+
+b = h;
+for (i = 0; b; i++)
+{
+a = b->str;
+if (a == NULL)
+{
+a = "(nil)";
+}
+b = b->next;
+}
+return (i);
 }
