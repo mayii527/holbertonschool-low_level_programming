7338b5bd2a339ba47b212f77dc14572471faa9a3
Mayii Cadavid <3434@holbertonschool.com>
2021-08-04 15:46:01 -0500

    files

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_list- a function that prints all
+ *             the elements of a list.
+ * @h: pointer start a list
+ * Return: number
+ */
+size_t print_listint(const listint_t *h);
+{
+        size_t i;
+        for (i = 0; h != NULL; i++)
+        {
+                printf("[%i]\n", h->len);
+        }
+        h = h->next;
+}
+return (i);
+}
