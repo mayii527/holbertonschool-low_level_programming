5e10eea44966f69a1f458236ab80a200de353efc
Mayii Cadavid <3434@holbertonschool.com>
2021-08-04 15:48:59 -0500

    correccion

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,9 +10,9 @@ size_t print_listint(const listint_t *h);
         size_t i;
         for (i = 0; h != NULL; i++)
         {
-                printf("[%i]\n", h->len);
+                printf("%i\n", h->len);
+                h = h->next;
         }
-        h = h->next;
 }
 return (i);
 }
