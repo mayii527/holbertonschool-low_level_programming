8a8f7e8b288afbb9edbf7a24c9377063c07d3cd7
Mayii Cadavid <3434@holbertonschool.com>
2021-08-04 10:24:53 -0500

    point 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 /**
  * print_list- a function that prints all 
  *             the elements of a list.
@@ -9,4 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
+        size_t i;
+
+        for (i = 0; h != NULL; i++)
+        {
+                if (h->str == NULL)
+                {
+                        printf("[0] (nil)");
+                }
+                else
+                {
+                        printf("[%i] %s", h->len, h->str);
+                }
+                h = h->next;
+        }
+        return (i);
 }
