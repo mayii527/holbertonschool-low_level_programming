b4e3bef3f8d1b1a55d86c1f82c446112699c1f44
mayii527 <3434@holbertonschool.com>
2021-08-04 17:38:42 -0700

    add nodes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,25 +1,26 @@
 #include "lists.h"
 /**
  * add_node - that returns the number of elements in a linked.
- * : structure
+ * @head: structure
+ * @str: algo
  * Return: number  of the elements
  */
 list_t *add_node(list_t **head, const char *str)
 {
-        list_t *new;
-        int i = 0;
-        new = malloc(sizeof(list_t));
-        if (new == NULL)
-        {
-                return (NULL);
-        }
-        while (str[i] != '\0')
-        {
-                i++;
-        }
-        new->str = strdup(str);
-        new->len = len;
-        new->next = head;
-        (*head) = new;
-        return (new);
+list_t *new;
+int i = 0;
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+while (str[i] != '\0')
+{
+i++;
+}
+new->str = strdup(str);
+new->len = i;
+new->next = *head;
+(*head) = new;
+return (new);
 }
