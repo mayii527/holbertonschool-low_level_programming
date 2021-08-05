6a169dc626faa1a2daf48e4f3972d655b1fb3031
mayii527 <3434@holbertonschool.com>
2021-08-04 19:51:58 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,11 +6,11 @@
  */
 void free_list(list_t *head)
 {
-    if (head == NULL)
-    {
-        return;
-    }
-    free_list(head->next);
-    free(head->str);
-    free(head);
+if (head == NULL)
+{
+return;
+}
+free_list(head->next);
+free(head->str);
+free(head);
 }
