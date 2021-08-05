8682714c7a596fff797cf3c9e4946c61c36ea7d4
mayii527 <85380406+mayii527@users.noreply.github.com>
2021-08-04 23:31:53 -0500

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,18 +6,11 @@
  */
 size_t listint_len(const listint_t *h)
 {
-int i;
-char *a;
-const listint_t *b = NULL;
-b = h;
-for (i = 0; b; i++)
-{
-a = b->str;
-if (a == NULL)
-{
-a = "(nil)";
-}
-b = b->next;
-}
-return (i);
+size_t i = 0;
+
+	for (; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);;
 }
