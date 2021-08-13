c57b0478bf89de0e93bdd6a326c0430031852e79
Mayii Cadavid <3434@holbertonschool.com>
2021-08-12 21:30:43 -0500

    proyect file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - that reads a text file and 
+ *                 prints it to the POSIX standard output. 
+ * 
+ * 
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  int fd;
+  ssize_t nr, wr;
+  char *buf;
+
+  if (filename == NULL)
+  {
+      return (0);
+  }
+
+  fd = open (filename, O_RDONLY);
+  if (fd == -1)
+  {
+      return (0);
+  }
+
+  buf = malloc(sizeof(char) * letters);
+  if (buf == NULL)
+  {
+      return (0);
+  }
+  nr = read(fd, buf, letters);
+  if (nr == -1)
+  {
+      return (0);
+  }
+close (fd);
+
+wr = write(STDOUT_FILENO, buf, nr);
+free (buf);
+return(wr);
+   
+}
