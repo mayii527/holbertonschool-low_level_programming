4704f6d135153106745cf9324334167b205d76aa
mayii527 <3434@holbertonschool.com>
2021-08-13 08:02:25 -0700

    correcion betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,39 @@
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
-
 /**
- * read_textfile - that reads a text file and 
- *                 prints it to the POSIX standard output. 
- * 
- * 
+ * read_textfile - reads a text file and  prints
+ *                 it to the POSIX standard output.
+ *@filename: name of file
+ *@letters: number of letters
+ * Return: wr
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  ssize_t nr, wr;
-  char *buf;
-
-  if (filename == NULL)
-  {
-      return (0);
-  }
-
-  fd = open (filename, O_RDONLY);
-  if (fd == -1)
-  {
-      return (0);
-  }
-
-  buf = malloc(sizeof(char) * letters);
-  if (buf == NULL)
-  {
-      return (0);
-  }
-  nr = read(fd, buf, letters);
-  if (nr == -1)
-  {
-      return (0);
-  }
-close (fd);
-
+int fd;
+ssize_t nr, wr;
+char *buf;
+if (filename == NULL)
+{
+return (0);
+}
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+return (0);
+}
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+{
+return (0);
+}
+nr = read(fd, buf, letters);
+if (nr == -1)
+{
+return (0);
+}
+close(fd);
 wr = write(STDOUT_FILENO, buf, nr);
-free (buf);
-return(wr);
-   
+free(buf);
+return (wr);
 }
