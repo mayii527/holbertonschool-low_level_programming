c187951d689d3edcbdfa02331bca63248150ac72
Mayii <3434@holbertonschool.com>
2021-12-06 17:25:25 -0500

    add task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,3 +4,18 @@
 
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
+    perimetro = 0
+    row = len(grid)
+    col = len(grid[0])
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] != 1:
+                    perimetro += 1
+                if grid[i + 1][j] != 1:
+                    perimetro += 1
+                if grid[i][j - 1] != 1:
+                    perimetro += 1
+                if grid[i][j + 1] != 1:
+                    perimetro += 1
+    return perimetro 
